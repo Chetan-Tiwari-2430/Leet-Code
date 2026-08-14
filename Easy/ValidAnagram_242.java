305a83625b077fafed900c3bedda070a0ddcb977
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-08-14 19:32:07 +0530

    14/Aug/2026

---
+++ Easy/ValidAnagram_242.java
@@ -0,0 +1,19 @@
+class ValidAnagram_242 {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] nums= new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            nums[s.charAt(i) - 'a']++;
+            nums[t.charAt(i) - 'a']--; 
+        }
+        for(int j : nums){
+            if(j != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
