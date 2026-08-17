690cf6f47f68052011ddca2ad764ae8ae1f119d8
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-08-17 22:47:02 +0530

    17-Aug-2026

---
+++ Easy/ValidParenthesis_20.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+class ValidParenthesis_20 {
+    public boolean isValid(String s) {
+        if(s.length() == 1){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+            }
+                if(ch == '}' && stack.pop() != '{' ||
+                   ch == ')' && stack.pop() != '(' ||
+                   ch == ']' && stack.pop() != '['  ){
+                        return false;
+                   }
+            
+        }
+        return stack.isEmpty();
+    }
+}
