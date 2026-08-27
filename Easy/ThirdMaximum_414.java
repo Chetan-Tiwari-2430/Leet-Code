7383aa92fa44a83bee90046cac2837eea1681c0b
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-08-27 14:16:25 +0530

    27-Aug-2026

---
+++ Easy/ThirdMaximum_414.java
@@ -0,0 +1,24 @@
+class ThirdMaximum_414 {
+    public int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+        long third = Long.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > first) {
+                third = second;
+                second = first;
+                first = nums[i];
+            } else if (nums[i] > second && nums[i] != first) {
+                third = second;
+                second = nums[i];
+            } else if (third < nums[i] && nums[i] != second && nums[i] != first) {
+                third = nums[i];
+            }
+        }
+        if(third == Long.MIN_VALUE){
+            return (int)first;
+        }
+        return (int) third;
+    }
+}
