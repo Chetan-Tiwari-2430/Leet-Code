9d5fdff0228da2a688908c6d264fe8881f1dc83f
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-08-29 22:08:34 +0530

    29-Aug-2026

---
+++ Easy/LengthOfLastWord_58.java
@@ -0,0 +1,13 @@
+class LengthOfLastWord_58 {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int count = 0;
+        for(int i = s.length()-1; i >= 0; i--){
+            if(s.charAt(i) == ' '){
+                return count;
+            }
+            count++;
+        }
+        return s.length();
+    }
+}
